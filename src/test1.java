faf9a3deed043d293b89371d16db735cdb155078
satı demirci <69872891+satdem@users.noreply.github.com>
2020-11-25 20:29:45 +0100

    ekleme sepet kısmı

--- src/test1.java
+++ src/test1.java
@@ -1,5 +1,6 @@
 public class test1 {
     public static void main(String[] args) {
         System.out.println("ürünleri istele yapıldı");
+        System.out.println("sepete eklekısmı yapıldı");
     }
 }
