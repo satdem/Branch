7ac9ce63d1fd1b6f421022a0d35bf4020304a5ea
satı demirci <69872891+satdem@users.noreply.github.com>
2020-11-25 20:37:48 +0100

    ekleme sepet kısmı altarnatif

--- src/test1.java
+++ src/test1.java
@@ -1,6 +1,7 @@
 public class test1 {
     public static void main(String[] args) {
         System.out.println("ürünleri istele yapıldı");
-        System.out.println("sepete eklekısmı yapıldı");
+        System.out.println("sepete yeni ekle kısmı yapıldı");
+        System.out.println("sepete yeni ekle 2. kısmı yapıldı");
     }
 }
